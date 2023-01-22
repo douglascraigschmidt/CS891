9d4f31548bdfecacb540425d34766b0d5852aa20
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-01-22 17:36:41 -0600

    review comments

---
+++ assignment1b/movies/src/main/java/edu/vandy/recommender/movies/server/MoviesService.java
@@ -0,0 +1,260 @@
+package edu.vandy.recommender.movies.server;
+
+import edu.vandy.recommender.movies.common.model.Movie;
+import edu.vandy.recommender.movies.utils.FutureUtils;
+import jdk.incubator.concurrent.StructuredTaskScope;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+
+/**
+ * This class defines implementation methods that are called by the
+ * {@link MoviesController} that to return a {@link List} of objects
+ * containing information about movies.
+ *
+ * This class is annotated as a Spring {@code @Service}, which enables
+ * the automatic detection and wiring of dependent implementation
+ * classes via classpath scanning.  It also includes its name in the
+ * {@code @Service} annotation below so that it can be identified as a
+ * service.
+ */
+@Service
+public class MoviesService {
+    /**
+     * This auto-wired field connects the {@link MoviesService} to the
+     * {@link List} of {@link Movie} objects.
+     */
+    // TODO -- ensure that mMovies is autowired with the appropriate
+    // @Bean factory method.
+    List<Movie> mMovies;
+
+    /**
+     * @return A {@link List} of all the movies
+     */
+    public List<Movie> getMovies() {
+        // TODO -- you fill in here, replacing 'return null' with
+        // the proper code.
+        return null;
+    }
+
+    /**
+     * Search for movie titles containing the given query {@link
+     * String} using the Java sequential streams framework.
+     *
+     * @param regexQuery The search query in regular expression form
+     * @return A {@link List} of {@link Movie} objects containing the
+     *         query
+     */
+    public List<Movie> search(String regexQuery) {
+        // Call the overloaded search(List<String>) method to locate
+        // all movies whose 'id' matches the 'query' and return them
+        // as a List of Movie objects.
+
+        // TODO -- you fill in here, replacing 'return null' with
+        // the proper code.
+        return null;
+    }
+
+    /**
+     * Search for movie titles containing the given query {@link
+     * String} using the Java sequential streams framework.
+     *
+     * @param regexQueries The search queries in regular expression
+     *                     form
+     * @return A {@link List} of {@link Movie} objects containing the
+     *         queries
+     */
+    public List<Movie> search(List<String> regexQueries) {
+        // Use Java structured concurrency to locate all movies whose
+        // 'id' matches the List of 'regexQueries' and return them as
+        // a List of Movie objects.
+
+        // Convert the 'regexQueries' into a List of Pattern objects.
+        // TODO -- you fill in here, replacing 'null' with the proper
+        // code.
+        List<Pattern> patternList = null;
+
+        try (// Create a new StructuredTaskScope that shutsdown on
+             // failure.
+             // TODO -- You fill in here, replacing 'null' with
+             // the proper code.
+             StructuredTaskScope.ShutdownOnFailure scope = null
+             ) {
+
+            // Call a helper method to concurrently get a List of all
+            // Movie objects that match the patternList.
+             // TODO -- You fill in here, replacing 'null' with
+             // the proper code.
+            List<Future<List<Movie>>> results = null;
+
+            // Perform a barrier synchronization that waits for all
+            // the tasks to complete.
+            // TODO -- you fill in here.
+
+            // Throw an Exception upon failure of any tasks.
+            // TODO -- you fill in here.
+
+            // Call a helper method that concatenates all matches and
+            // returns a List of Movie objects that matched at least
+            // one client query.
+            // TODO -- you fill in here, replacing 'return null' with
+            // the proper code.
+            return null;
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception.getMessage());
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * Convert the {@link List} of {@code regexQueries} into a {@link
+     * List} of compiled regular expression {@link Pattern} objects
+     *
+     * @param regexQueries The {@link List} of regular expression
+     *                     queries
+     * @return a {@link List} of compiled regular expression {@link
+     *         Pattern} objects
+     */
+    private List<Pattern> makePatternList
+        (List<String> regexQueries) {
+        // Use a Java sequential stream to perform the following
+        // steps:
+        //
+        // 1. Convert the List to a Stream.
+        // 
+        // 2. Decode each regular expression.
+        // 
+        // 3. Compile regex patterns that ignore case.
+        // 
+        // 4. Convert the Stream to a List and return that List.
+
+        // TODO -- you fill in here, replacing 'return null' with the
+        // proper code.
+        return null;
+    }
+
+    /**
+     * Concurrently get a {@link List} of all {@link Movie} objects
+     * that match the {@code patternList}.
+     *
+     * @param patternList A {@link List} of queries in compiled
+     *                    regular expression form
+     * @param scope The {@link StructuredTaskScope} used to {@code
+     *              fork()} a virtual thread
+     * @return A {@link List} of {@link Future} objects that will emit
+     *         a {@link List} of {@link Movie} objects that match
+     *         queries in {@code patternList}
+     */
+    @NotNull
+    private List<Future<List<Movie>>> getMatches
+        (List<Pattern> patternList,
+         StructuredTaskScope.ShutdownOnFailure scope) {
+        // Use a Java sequential stream to perform the following
+        // steps:
+        //
+        // 1. Convert List to a Stream.
+        // 
+        // 2. Call a helper method that uses Java StructuredTaskScope
+        //    to concurrently determine if the movie title matches
+        //    with each regex query.
+        // 
+        // 3. Convert the Stream to a List and return that List.
+
+        // TODO -- you fill in here, replacing 'return null' with the
+        // proper code.
+        return null;
+    }
+
+    /**
+     * Use the Java {@link StructuredTaskScope.ShutdownOnFailure}
+     * to concurrently determine if the {@code pattern} matches
+     * any {@link Movie} objects.
+     *
+     * @param pattern The search query in compiled regular expression
+     *                form
+     * @param scope The {@link StructuredTaskScope} used to {@code
+     *              fork()} a virtual thread
+     * @return A {@link Future} to an {@link List} of matching {@link
+     *         Movie} objects
+     */
+    private Future<List<Movie>> findMatches
+        (Pattern pattern,
+         StructuredTaskScope.ShutdownOnFailure scope) {
+        // Use the 'scope' parameter to determine if the 'pattern'
+        // matches any Movie objects in the 'mMovies' as follows:
+        // 
+        // 1. Create a virtual thread to perform processing
+        //    concurrently.
+        // 
+        // 2. Convert the mMovies List to a Stream.
+        //
+        // 3. Call a helper method and only keeps Movie objects that
+        //    match the pattern.
+        // 
+        // 4. Convert the Stream of (any) matches to a List and return
+        //    that List.
+
+        // TODO -- you fill in here, replacing 'return null' with
+        // the proper code.
+        return null;
+    }
+
+    /**
+     * Find a match between the {@code pattern} and the {@code movie}.
+     *
+     * @param pattern The query in compiled regular expression form
+     * @param movie The {@link Movie} to match with
+     * @return True if there's a match, else false
+     */
+    private boolean match(Pattern pattern,
+                          Movie movie) {
+        // Find a match between the 'pattern' and the 'movie' by
+        // performing the following steps:
+        // 
+        // 1. Create a Matcher for the movie's title.
+        // 
+        // 2. Call a Matcher method that returns true if there's a
+        //    match, else false.
+
+        // TODO -- you fill in here, replacing 'return false' with
+        // the proper code.
+        return false;
+    }
+
+    /**
+     * Get a {@link List} of {@link Movie} objects that matched at
+     * least one client query.
+     *
+     * @param results A {@link List} of {@link Future} objects
+     *                containing a {@link List} of {@link Movie}
+     *                objects that matched client queries
+     * @return A {@link List} of matching {@link Movie} objects
+     */
+    @NotNull
+    private static List<Movie> concatMatches
+        (List<Future<List<Movie>>> results) {
+        // Use a Java sequential stream to perform the following
+        // steps:
+        //
+        // 1. Convert the List of Future<List<Movie>> objects to a
+        //    Stream of List<Movie> objects (see the FutureUtils
+        //    helper class).
+        // 
+        // 2. Flatten the Stream of List objects into a single Stream
+        //    (also eliminating empty List objects).
+        // 
+        // 3. Convert the Stream to a List of Movie objects that are
+        //    returned from the method.
+
+        // TODO -- you fill in here, replacing 'return null' with
+        // the proper code.
+        return null;
+    }
+}
