a365eae3f62002cf9fba8b5fc15fbbe01c5e6866
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-03-12 11:20:09 -0500

    review comments

---
+++ assignment1a/movies/src/main/java/edu/vandy/recommender/movies/EurekaConfiguration.java
@@ -0,0 +1,18 @@
+package edu.vandy.recommender.movies;
+
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @@ Monte, what is the purpose of this class?
+ */
+/*
+@Profile("eureka")
+@Configuration
+@EnableDiscoveryClient
+public class EurekaConfiguration {
+
+}
+
+ */
